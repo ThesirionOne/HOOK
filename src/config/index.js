2474fa399fd08c43cefca1011b6ac1352d9ab9cf
ThesirionOne <152097635+ThesirionOne@users.noreply.github.com>
2024-12-30 04:58:51 +0100

    Delete src directory

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
-module.exports = {
-  telegramToken: process.env.TELEGRAM_BOT_TOKEN,
-  quicknodeUrl: process.env.QUICKNODE_HTTP_URL,
-  port: process.env.PORT || 3000
-};
